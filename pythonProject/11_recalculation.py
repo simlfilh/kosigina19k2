c954f382f6cb663ccc75b93be8ba82553bc88dfc
Valeria <148010378+simlfilh@users.noreply.github.com>
2025-06-16 18:48:12 +0300

    Update 11_recalculation.py

--- pythonProject/11_recalculation.py
+++ pythonProject/11_recalculation.py
@@ -31,7 +31,7 @@ st.markdown("""
         .highlight-blue {
             background-color: #B3E5FC;
             border-radius: 10px;
-            padding: 20px;
+            padding: 15px;
             margin-bottom: 20px;
             position: relative;
         }

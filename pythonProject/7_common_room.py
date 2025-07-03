62c37c72ae69464a974b193469cf8612c07dc203
Valeria <148010378+simlfilh@users.noreply.github.com>
2025-07-03 15:55:19 +0300

    Update 7_common_room.py

--- pythonProject/7_common_room.py
+++ pythonProject/7_common_room.py
@@ -100,6 +100,17 @@ with col5:
             <p>ПТ: 14:00 — 17:00</p>
         </div>
                 """, unsafe_allow_html=True)
+    
+st.markdown("""
+    <div class="colored-container">
+            <div class="highlight-blue">
+                <div class="text-indent-content">
+                    <h3>🔐 Камера хранения | 1 ЭТАЖ</h3> 
+                </div>
+            </div>
+    </div>
+            """, unsafe_allow_html=True)
+st.divider()
 
 st.markdown("""
     <div class="colored-container">

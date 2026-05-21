f9d076a1a0d79ed78666e03b811e5b4ce5075c13
Valeria <148010378+simlfilh@users.noreply.github.com>
2026-05-21 13:32:20 +0300

    Update 0_pages.py

--- pythonProject/0_pages.py
+++ pythonProject/0_pages.py
@@ -43,8 +43,8 @@ pages = {
     "📍 ЧТО ЕСТЬ В ОБЩЕЖИТИИ?": [
         st.Page("6_block.py", title="— Блоки для проживания"),
         st.Page("7_common_room.py", title="— Бытовое пространство"),
-        st.Page("8_student_room.py", title="— Студенческое пространство"),
-        st.Page("9_guests_room.py", title="— Гостевые блоки")
+        st.Page("8_student_room.py", title="— Студенческое пространство")
+        # st.Page("9_guests_room.py", title="— Гостевые блоки")
     ],
     "📋 ДОКУМЕНТЫ": [
         st.Page("10_payment.py", title="— Оплата проживания"),
